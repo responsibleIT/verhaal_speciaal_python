ea0e5e333c7d1a1a42677786533921ca8514564d
Michiel Bontenbal <michiel.bontenbal@gmail.com>
2025-05-27 13:59:56 +0200

    updates claude

---
+++ leesniveaus.py
@@ -0,0 +1,46 @@
+
+#variables based on the reading level settings
+level_one_old = 'Leesniveau 1: Tekst bestaat uit korte woorden die je precies zo schrijft zoals je ze uitspreekt. Voorbeelden hiervan zijn maan, bos, man, roos. Er mogen dus geen woorden voorkomen met bijvoorbeeld sch- en -ng en -nk, -b, -d(t), -ch(t), -ooi, -aai, -oei, -eeuw, -ieuw, -uw. De zinnen zijn zo kort mogelijk. Elke zin begint op een nieuwe regel. Er komen geen in hoofdletters voor, dus alle woorden worden met kleine letters geschreven.'
+
+level_two_old = 'Leesniveau 2: Tekst bestaat uit één- en tweelettergrepige woorden. Woorden met -sch en -ng mogen voorkomen.  Woorden mogen voorkomen eindigend op -nk, -b, -d(t), -ch(t), -ooi, -aai, -oei, -eeuw, -ieuw, -uw. Een-. Ook mogen tweelettergrepige woorden met twee of drie medeklinkers na elkaar zoals staart, botst, sprong, bankje, knappe, winkel  Er mogen ook enkele makkelijke drielettergrepige woorden voorkomen, zoals sinterklaas. De zinnen zijn meestal kort. Soms mogen samengestelde zinnen voorkomen. Hoofdletters worden gebruikt.'
+
+level_three_old = 'Leesniveau 3: Er mogen woorden gebruikt worden met 3 of meer lettergrepen. De zinnen mogen langer zijn. De zinnen kunnen bestaan uit een hoofdzin met een bijzin. Gebruik van leenwoorden, zoals bureau, horloge, chauffeur is beperkt toegestaan. Ook eenvoudige leesmoeilijkheden van leenwoorden komen voor: i en y uitgesproken als ie; c uitgesproken als k of als s. Zinnen mogen beginnen op dezelfde regel. Samengestelde zinnen mogen voorkomen. Hoofdletters worden gebruikt.'
+
+level_four_old = 'Leesniveau 4: Er zijn geen beperkingen in woorden en zinslengte. Lastig te lezen leenwoorden (gamen), onbekende woorden (ov-pas, ornament) en leestekens (ideeën, ruïne) komen meer voor. Woorden eindigend op -ele, -eaal, -ueel, -iaal of -ieel komen voor. Ook woorden met een trema komen voor. Ook woorden beginnend met /ch/ uitgesproken als /sj/, eindigend op –ge, uitgesproken als /zje/, eindigend op –isch, woorden met klinkerreeks, leenwoorden met eau, é of è. Hoofdletters worden gebruikt.'
+
+
+#variables based on the reading level settings
+level_one = '''Leesniveau 1: 
+Tekst bestaat uit korte woorden die je precies zo schrijft zoals je ze uitspreekt. Voorbeelden hiervan zijn maan, bos, man, roos. 
+Er mogen dus geen woorden voorkomen met bijvoorbeeld sch- en -ng en -nk, -b, -d(t), -ch(t), -ooi, -aai, -oei, -eeuw, -ieuw, -uw. 
+De zinnen zijn zo kort mogelijk. 
+Elke zin begint op een nieuwe regel. 
+Er komen geen in hoofdletters voor, dus alle woorden worden met kleine letters geschreven.'
+'''
+
+level_two = '''Leesniveau 2: 
+Tekst bestaat uit één- en tweelettergrepige woorden. 
+Woorden met -sch en -ng mogen voorkomen.  
+Woorden mogen voorkomen eindigend op -nk, -b, -d(t), -ch(t), -ooi, -aai, -oei, -eeuw, -ieuw, -uw. Een-. 
+Ook mogen tweelettergrepige woorden met twee of drie medeklinkers na elkaar zoals staart, botst, sprong, bankje, knappe, winkel. 
+Er mogen ook enkele makkelijke drielettergrepige woorden voorkomen, zoals Sinterklaas. 
+De zinnen zijn meestal kort, maar samengestelde zinnen kunnen voorkomen. 
+Hoofdletters worden gebruikt.'
+'''
+
+level_three = '''Leesniveau 3: 
+Er mogen woorden gebruikt worden met 3 of meer lettergrepen. 
+De zinnen mogen langer zijn. 
+De zinnen kunnen bestaan uit een hoofdzin met een bijzin. 
+Gebruik van leenwoorden, zoals bureau, horloge, chauffeur is bij uitzondering toegestaan. 
+Ook eenvoudige leenwoorden komen voor: i en y uitgesproken als ie; c uitgesproken als k of als s, gamen 
+Hoofdletters worden gebruikt.'
+'''
+level_four = '''Leesniveau 4: 
+Samengestelde zinnen komen voor. 
+Lastige leenwoorden zijn ook toegestaan.
+Speciale leestekens  (ideeën, ruïne, saté, Curaçao) komen meer voor. 
+Woorden eindigend op -ele, -eaal, -ueel, -iaal of -ieel komen voor. 
+Ook woorden beginnend met /ch/ uitgesproken als /sj/, eindigend op –ge, uitgesproken als /zje/, eindigend op –isch, woorden met klinkerreeks, leenwoorden met eau, é of è. 
+Hoofdletters worden gebruikt.
+'''
